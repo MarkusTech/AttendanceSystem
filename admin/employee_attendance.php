de8fa38812ac4a52dfaae4581bd6c40c73fa3b02
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-11-22 02:36:05 +0800

    update

--- admin/employee_attendance.php
+++ admin/employee_attendance.php
@@ -48,7 +48,7 @@ include("controller.php");
                         <span class="hidden-xs"><?php echo $_SESSION['name']; ?></span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                         <span class="dropdown-item dropdown-header" style="max-height: 150px; overflow:hidden; background:#222d32;">
+                        <span class="dropdown-item dropdown-header" style="max-height: 150px; overflow:hidden; background:#222d32;">
                             <div class="image">
                                 <img src="dist/img/favicon.ico" style="border-radius: 50%;width: 100x;height: 100px;" alt="User Image">
                             </div>
--- admin/employee_list.php
+++ admin/employee_list.php
@@ -163,9 +163,6 @@ include("controller.php");
 
         <div class="content-wrapper">
             <div class="content-header">
-                <div style="padding-top: 10px;">
-                    <marquee onMouseOver="this.stop()" onMouseOut="this.start()"> <a href="https://www.youtube.com/@codecampbdofficial">Code Camp BD</a> is the sole owner of this script. It is not suitable for personal use. And releasing it in demo version. Besides, it is being provided for free only from <a href="https://www.youtube.com/@codecampbdofficial">Code Camp BD</a>. For any of your problems contact us on <a href="https://www.youtube.com/@codecampbdofficial">Code Camp BD</a> facebook group / page or message <a href="https://www.facebook.com/dev.mhrony">MH RONY</a> on facebook. Thanks for staying with <a href="https://www.youtube.com/@codecampbdofficial">Code Camp BD</a>.</marquee>
-                </div>
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
