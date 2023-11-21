d0bf4e1e0ccccfa30292c5b083e5db666b9efb91
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-11-22 02:07:12 +0800

    Update timestamp.php

--- timestamp.php
+++ timestamp.php
@@ -1,17 +1,4 @@
-<!--- Author By: MH RONY
-Author Website: https://developerrony.com
-Github Link: https://github.com/dev-mhrony
-Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
--->
-
-
 <?php
 
 date_default_timezone_set('Asia/Manila');
 $time = date("h:i:s");
-
-
-// <!--- Author By: MH RONY
-// Author Website: https://developerrony.com
-// Github Link: https://github.com/dev-mhrony
-// Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
